3f5dd326c71064fb61c25f67560f39260ba90693
Shubham Chopra <shubham.chopra2906@gmail.com>
2017-02-18 21:50:16 +0530

    Update Source.cpp

--- Week 1/Optional Assignment 2/Source.cpp
+++ Week 1/Optional Assignment 2/Source.cpp
@@ -1,48 +1,5 @@
-#include <stdio.h>
-#include <opencv2\highgui.hpp>
-#include <opencv2\core.hpp>
-#include <opencv2\opencv.hpp>
+// Without using OpenCV's built in functions. This code describes a Mask and applies it to the Image pixel by pixel.
 
-#include <iostream>
-
-using namespace std;
-using namespace cv;
-
-int main(int argc, char** argv)
-{
-	Mat image, I, gray, Res;
-	image = imread("apple2.jpg", 1);
-
-	// For taking it via Command Line
-	// image = imread(argv[1], 1); 
-
-	// Converting to Gray Scale : If Required
-	cvtColor(image, I, cv::COLOR_BGR2GRAY);
-
-	blur(I, Res, Size(20, 20));
-
-	// Display Images
-	cvNamedWindow("Original Image", CV_WINDOW_AUTOSIZE);
-	imshow("Original Image", I);
-
-	cvNamedWindow("Smoothed Image", CV_WINDOW_AUTOSIZE);
-	imshow("Smoothed Image", Res);
-
-	cv::waitKey(0);
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-/*
 #include <stdio.h>
 #include <opencv2\highgui.hpp>
 #include <opencv2\core.hpp>
@@ -133,4 +90,3 @@ int main(int argc, char** argv)
 	cv::waitKey(0);
 	return 0;
 }
-*/
